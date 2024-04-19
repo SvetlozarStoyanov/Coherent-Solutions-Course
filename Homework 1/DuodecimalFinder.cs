81aa114b5a7be1fd9afabf6bd537597a599a45e8
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-19 12:48:25 +0300

    Added class which returns numbers who fulfil given condition

---
+++ Homework 1/DuodecimalFinder.cs
@@ -0,0 +1,58 @@
+﻿namespace Homework_1
+{
+    internal class DuodecimalFinder
+    {
+        public DuodecimalFinder()
+        {
+
+        }
+
+        public string GetNumbersWhichContainSubstring(int num01,
+            int num02,
+            string substring = "AA")
+        {
+            return string.Join(", ", FindNumbersWhichContainSubstring(num01,
+                num02,
+                substring));
+        }
+
+        private List<int> FindNumbersWhichContainSubstring(int num01,
+            int num02,
+            string substring)
+        {
+            var numbersWhichMatchCondition = new List<int>();
+            if (num01 < num02)
+            {
+                for (int i = num01; i <= num02; i++)
+                {
+                    if (ConvertToDuodecimal(i).Contains(substring))
+                    {
+                        numbersWhichMatchCondition.Add(i);
+                    }
+                }
+            }
+            return numbersWhichMatchCondition;
+        }
+
+        private string ConvertToDuodecimal(int number)
+        {
+            string duodecimalChars = "0123456789AB";
+            string result = string.Empty;
+
+            if (number == 0)
+            {
+                return "0";
+            }
+
+            while (number > 0)
+            {
+                int remainder = number % 12;
+
+                result = duodecimalChars[remainder] + result;
+
+                number /= 12;
+            }
+            return result;
+        }
+    }
+}
