e723deb45a77e60fcba23293023aafe0e53ca072
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-19 12:47:17 +0300

    Added class which adds check digit to ISBN

---
+++ Homework 1/ISBNWriter.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework_1
+{
+    internal class ISBNWriter
+    {
+        public ISBNWriter()
+        {
+
+        }
+
+        public void AddControlDigitToISBN(ref string input)
+        {
+            var checkDigit = 0.0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                checkDigit += (i + 1) * (Convert.ToInt32(input[i]) - 48);
+            }
+            checkDigit = checkDigit % 11;
+            if (checkDigit != 10)
+            {
+                input += "-" + checkDigit;
+            }
+            else
+            {
+                input += "-X";
+            }
+        }
+    }
+}
