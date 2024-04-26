73d1d28d04f8eebd580ad4f35bb12fd67d09d5c5
SvetlozarStoyanov <svetlio214@gmail.com>
2024-04-26 16:54:07 +0300

    Changed to fulfil task description.

--- Homework 1/ISBNWriter.cs
+++ Homework 1/ISBNWriter.cs
@@ -12,10 +12,13 @@
             var checkDigit = 0.0;
             for (int i = 0; i < input.Length; i++)
             {
-                checkDigit += (i + 1) * (Convert.ToInt32(input[i]) - 48);
+                // input[i] - 48 -> gives us the current digit.
+                // Subtracting is necessary because input[i] is a char and that adds an additional 48 to its value
+                checkDigit += (10 - i) * (input[i] - 48);
             }
+            checkDigit = checkDigit + checkDigit;
             checkDigit = checkDigit % 11;
-            if (checkDigit != 10)
+            if (checkDigit > 0)
             {
                 input += "-" + checkDigit;
             }
