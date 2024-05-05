b2808c32c61465c5a76381d2bc46af5e7a463dde
SvetlozarStoyanov <svetlio214@gmail.com>
2024-05-05 18:33:34 +0300

    Add method now adds elements from bigger matrix if there is a size difference

--- Homework 2/Task 2.2/DiagonalMatrixExtentions.cs
+++ Homework 2/Task 2.2/DiagonalMatrixExtentions.cs
@@ -6,14 +6,15 @@
         {
             var minSize = Math.Min(matrixOne.Size, matrixTwo.Size);
             var newSize = matrixOne.Size < matrixTwo.Size ? matrixTwo.Size : matrixOne.Size;
+            var biggerMatrix = matrixOne.Size < matrixTwo.Size ? matrixTwo.DiagonalItems : matrixOne.DiagonalItems;
             var resultMatrixDiagonalArray = new int[newSize];
             for (int i = 0; i < minSize; i++)
             {
                 resultMatrixDiagonalArray[i] = matrixOne[i, i] + matrixTwo[i, i];
             }
-            for (int i = minSize; i < resultMatrixDiagonalArray.Length; i++)
+            for (int i = minSize; i < biggerMatrix.Length; i++)
             {
-                resultMatrixDiagonalArray[i] = 0;
+                resultMatrixDiagonalArray[i] = biggerMatrix[i];
             }
             var resultMatrix = new DiagonalMatrix(resultMatrixDiagonalArray);
             return resultMatrix;
