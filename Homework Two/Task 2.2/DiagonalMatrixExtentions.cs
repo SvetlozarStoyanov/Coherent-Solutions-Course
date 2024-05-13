c579a0165ef2e04dc9cc8505edc72fcb51c5e862
SvetlozarStoyanov <svetlio214@gmail.com>
2024-05-13 15:48:37 +0300

    Readded files

---
+++ Homework Two/Task 2.2/DiagonalMatrixExtentions.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework_2.Task_2._2
+{
+    public static class DiagonalMatrixExtentions
+    {
+        public static DiagonalMatrix AddMatrix(this DiagonalMatrix matrixOne, DiagonalMatrix matrixTwo)
+        {
+            var minSize = Math.Min(matrixOne.Size, matrixTwo.Size);
+            var newSize = matrixOne.Size < matrixTwo.Size ? matrixTwo.Size : matrixOne.Size;
+            var biggerMatrix = matrixOne.Size < matrixTwo.Size ? matrixTwo.DiagonalItems : matrixOne.DiagonalItems;
+            var resultMatrixDiagonalArray = new int[newSize];
+            for (int i = 0; i < minSize; i++)
+            {
+                resultMatrixDiagonalArray[i] = matrixOne[i, i] + matrixTwo[i, i];
+            }
+            for (int i = minSize; i < biggerMatrix.Length; i++)
+            {
+                resultMatrixDiagonalArray[i] = biggerMatrix[i];
+            }
+            var resultMatrix = new DiagonalMatrix(resultMatrixDiagonalArray);
+            return resultMatrix;
+        }
+    }
+}
---
+++ Homework Two/Task 2.3/SchoolOccupationBase.cs
@@ -0,0 +1,15 @@
+﻿using Homework_2.Task_2._3.Contracts;
+
+namespace Homework_2.Task_2._3
+{
+    public abstract class SchoolOccupationBase : ICloneable<SchoolOccupationBase>
+    {
+        public SchoolOccupationBase(string textDescription)
+        {
+            this.TextDescription = textDescription;
+        }
+        public string? TextDescription { get; set; }
+
+        public abstract SchoolOccupationBase Clone();
+    }
+}
